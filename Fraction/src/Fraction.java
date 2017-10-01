d3e416c4bedb749c22415e9baf668cc1e56da74d
hhvhhvcz <hhvhhvcz@gmail.com>
2017-10-01 22:35:14 +0200

    Made constructor (full) for fraction class (testing multiple commits and then push to GitHub)

--- Fraction/src/Fraction.java
+++ Fraction/src/Fraction.java
@@ -10,4 +10,14 @@ public class Fraction {
 	
 	private double numberator;
 	private double denominator;
+	
+	/**
+	 * 
+	 * @param numerator
+	 * @param denumerator
+	 */
+	public Fraction(double numerator, double denumerator) {
+		this.numberator = numberator;
+		this.denominator = denominator;
+	}
 }
