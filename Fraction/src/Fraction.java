31be798d9140e6db7f0ea23fbb93a54a5c41503f
hhvhhvcz <hhvhhvcz@gmail.com>
2017-10-02 22:22:11 +0200

    Completing basic operation
    Created methodes for basic operations

--- Fraction/src/Fraction.java
+++ Fraction/src/Fraction.java
@@ -4,27 +4,104 @@
 
 /**
  * @author Hahaw
- *
+ *	Fraction class
  */
 public class Fraction {
 	
-	private double numberator;
-	private double denominator;
+	private int numerator;
+	private int denominator;
 	
 	/**
-	 * 
-	 * @param numerator
-	 * @param denumerator
+	 * full constructor
+	 * @param numerator - value of numerator
+	 * @param denominator value of denominator
 	 */
-	public Fraction(double numerator, double denumerator) {
-		this.numberator = numberator;
+	public Fraction(int numerator, int denominator) {
+		if (denominator == 0) {
+			System.out.println("Jmenovatel nemuze byt nula!");
+			return;
+		}
+		this.numerator = numerator;
 		this.denominator = denominator;
+		this.greatestDivider();
 	}
 	/**
-	 * 
+	 * empty constructor
 	 */
 	public Fraction() {
-		numberator = 1.0;
-		denominator = 1.0;
+		numerator = 1;
+		denominator = 1;
+	}
+	
+	/**
+	 * method which shortens the fraction
+	 */
+	public void greatestDivider() {
+		if(denominator == 0) {
+			throw new java.lang.ArithmeticException("/ by zero");
+		}
+		int a = numerator;
+		int b = denominator;
+		int divider = 0;
+		while(a!=0 && b!=0) {
+			divider = b;
+	        b = a%b;
+	        a = divider;
+	    }
+	    divider = a + b;
+	    
+	    numerator =  numerator / divider;
+	    denominator /= divider;
+		
+	}
+	/**
+	 * Method for adding two fraction
+	 * @param no1 - first fraction in operation
+	 * @param no2 - second fraction in operation
+	 * @return new fraction object after operation
+	 */
+	public static Fraction fractionAddition(Fraction no1, Fraction no2){
+		int num = no1.numerator * no2.denominator + no2.numerator * no1.denominator;
+		int denum = no1.denominator * no2.denominator;
+		return new Fraction(num, denum);
+	}
+	/**
+	 * Method for subtracting two fraction
+	 * @param no1 - first fraction in operation
+	 * @param no2 - second fraction in operation
+	 * @return  new fraction object after operation
+	 */
+	public static Fraction fractionSubtraction(Fraction no1, Fraction no2){
+		int num = no1.numerator * no2.denominator - no2.numerator * no1.denominator;
+		int denum = no1.denominator * no2.denominator;
+		return new Fraction(num, denum);
+	}
+	/**
+	 * Method for multiplying two fraction
+	 * @param no1 - first fraction in operation
+	 * @param no2 - second fraction in operation
+	 * @return  new fraction object after operation
+	 */
+	public static Fraction fractionMultiplication(Fraction no1, Fraction no2){
+		int num = no1.numerator * no2.numerator;
+		int denum = no1.denominator * no2.denominator;
+		return new Fraction(num, denum);
+	}
+	/**
+	 * Method for dividing two fraction
+	 * @param no1 - first fraction in operation
+	 * @param no2 - second fraction in operation
+	 * @return  new fraction object after operation
+	 */
+	public static Fraction fractionDivision(Fraction no1, Fraction no2){
+		int num = no1.numerator * no2.denominator;
+		int denum = no1.denominator * no2.numerator;
+		return new Fraction(num, denum);
+	}
+	/**
+	 * To string for writting the data in fraction to console
+	 */
+	public String toString() {
+		return "["+ numerator + "/" + denominator + "]";
 	}
 }
--- Fraction/src/Main.java
+++ Fraction/src/Main.java
@@ -13,7 +13,22 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Fraction f1 = new Fraction(20,40);
+		Fraction f2 = new Fraction(8,32);
+		
+		System.out.println(f1);
+		System.out.println(f2);
+		
+		Fraction add = Fraction.fractionAddition(f1, f2);
+		Fraction sub = Fraction.fractionSubtraction(f1, f2);
+		Fraction mul = Fraction.fractionMultiplication(f1, f2);
+		Fraction div = Fraction.fractionDivision(f1, f2);
+		
+		System.out.println(add);
+		System.out.println(sub);
+		System.out.println(mul);
+		System.out.println(div);
 	}
 
 }
